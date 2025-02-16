463050b4a9690fd1e801fb1941dfd358cf456fbd
suramyavns <sdidwania645@gmail.com>
2025-02-16 08:06:25 +0530

    moduling

--- src/app/home/settings/page.js
+++ src/app/home/settings/page.js
@@ -1,7 +1,22 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { auth } from "../../../../Firebase"
+import { signOut } from "firebase/auth"
+
 export default function Page() {
+    const router = useRouter()
+    const handleLogout = () => {
+        localStorage.clear();
+        signOut(auth)
+        .then(()=>{
+            router.replace('/')
+        })
+        .catch((e)=>{alert(e)})
+    }
     return (
         <div className="h-full w-11/12">
-            <button className="p-2 text-2xl rounded-lg w-full bg-slate-700">
+            <button onClick={handleLogout} className="p-2 text-2xl rounded-lg w-full bg-slate-700">
                 Logout
             </button>
         </div>
